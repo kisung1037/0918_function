202f9081d7da73a22e10f131e3b5bbc2774a6b70
Ki Seong <kisung1037@gmail.com>
2019-09-18 11:01:50 +0900

    commit

--- index/common/block.js
+++ index/common/block.js
@@ -4,7 +4,14 @@ window.onload = function() {
 }
 // 환경정보를 가진 객체
 var myGameArea = {
+    canvas = document.createElement("canvas"),
     message : {
         docIsReady: "문서가 준비됬습니다."
+    },
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 320;
+        //document.body.insertBefore(무엇을,어디에))
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
 }
