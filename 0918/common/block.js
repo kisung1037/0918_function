46e55caf847ce0fecea042db202c3a8c0d11121b
Ki Seong <kisung1037@gmail.com>
2019-09-19 16:17:26 +0900

    블록 업데이트

--- 0918/common/block.js
+++ 0918/common/block.js
@@ -2,10 +2,11 @@
 window.onload = function() {
     console.log(myGameArea.message.docIsReady);
     myGameArea.start();
-    this.myGamePiece = new Component(100,100,"red",10,10);
+    this.myGamePiece = new Component(30,30,"red",0,100);
 }
 
 var myGamePiece;
+// var myGamePieces;
 
 // 환경정보를 가진 객체
 var myGameArea = {
@@ -13,6 +14,7 @@ var myGameArea = {
     message : {
         docIsReady: "문서가 준비됬습니다."
     },
+    key : false,
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
@@ -21,6 +23,16 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         //타이머 적용
         this.interval = setInterval(updateGameArea, 20); // 초당 50번 updateGameArea를 호출
+        //마우스제어
+        this.btns = document.querySelectorAll(".controll >button");
+        this.btns[1].addEventListener("mousedown",moveLeft);
+        this.btns[1].addEventListener("mouseup",moveStop);
+        window.addEventListener("keydown", function(e){
+            myGameArea.key = e.keyCode;
+        });
+        window.addEventListener("keyup", function(e){
+            myGameArea.key = false;
+        });
     },
     clear : function () {
         this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
@@ -34,17 +46,50 @@ function Component(w,h,c,x,y) {
     this.c = c;
     this.x = x;
     this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+
     //외부 실행을 위해 함수에 포함
     this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = c;
         ctx.fillRect(this.x,this.y,this.w,this.h);
     }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
 
+// function refUp() {
+//     if (myGamePiece.x = 400) myGamePiece.update();
+// }
+function moveRight() {
+    myGamePiece.x = 1;
+}
+function moveLeft() {
+    myGamePiece.x =-1;
+}
+function moveStop() {
+    myGamePiece.x =0;
+    myGamePiece.y =0;
+}
 //화면 제어를 위한 함수1
 function updateGameArea(){
+    var mGAkey= myGameArea.key;
     myGameArea.clear();
-    myGamePiece.x += 1;
+    // myGamePiece.x += 1;
+    if (mGAkey && mGAkey === 37) myGamePiece.speedX = -1;
+    if (mGAkey && mGAkey === 39) myGamePiece.speedX = 1;
+    if (mGAkey && mGAkey === 38) myGamePiece.speedY = -1;
+    if (mGAkey && mGAkey === 40) myGamePiece.speedY = 1;
+    if (!mGAkey) myGameArea.clear();
+    myGamePiece.newPos();
     myGamePiece.update();
+    // for (var i=0;i<5;i++) {
+    //     myGamePieces.push([]);
+    //     for (j=0 ; j<3 ; j++){
+    //         myGamePieces[i].push(new Component(100,100,"red",10,10));
+    //     }
+    // }
 }
